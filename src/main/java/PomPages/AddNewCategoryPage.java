95b2d07d091d4e944fa8c3c2215765559cd01455
venne <venne@Vennela>
2023-11-08 12:24:50 +0530

    script was changed in AddNewCategory page

--- src/main/java/PomPages/AddNewCategoryPage.java
+++ src/main/java/PomPages/AddNewCategoryPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class AddNewCategoryPage {
+	//script is failed
 	
 	//Declaration
 	@FindBy(xpath="//b[text()='Add New Category']")
